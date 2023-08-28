6336ff59885385f0ae7dd21f95369b9dc0206876
alex <alexdok62@gmail.com>
2023-08-28 19:15:42 +0300

    fix

--- MyFavoritePlaces/ViewControllers/mainViewController/MainViewController.swift
+++ MyFavoritePlaces/ViewControllers/mainViewController/MainViewController.swift
@@ -1,5 +1,4 @@
 import UIKit
-import RealmSwift
 
 final class MainViewController: UIViewController {
     
--- MyFavoritePlaces/ViewControllers/mainViewController/MainViewControllerViewModel.swift
+++ MyFavoritePlaces/ViewControllers/mainViewController/MainViewControllerViewModel.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 class MainViewModel {
 
     let dataProvider = DataProviderImpl()
